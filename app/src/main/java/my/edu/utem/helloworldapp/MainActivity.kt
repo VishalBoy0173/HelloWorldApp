4904cdb71a2de3e1eece433c1fadcd72df7c3c3d
VishalBoy0173 <vishalboynathan111@gmail.com>
2026-01-17 02:12:49 +0800

    Initial Android project commit

---
+++ app/src/main/java/my/edu/utem/helloworldapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package my.edu.utem.helloworldapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import my.edu.utem.helloworldapp.ui.theme.HelloWorldApplicationTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldApplicationTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldApplicationTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/my/edu/utem/helloworldapp/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package my.edu.utem.helloworldapp.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
